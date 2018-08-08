c9b0528fdb80f665db7433c67cf99cf3fd11f186
Archesyn <Gdboy1996@live.co.uk>
2018-08-08 18:59:19 +0100

    Added the calling

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -7,6 +8,7 @@ namespace Test
         //Measurements of liquids
         public int Millilitre;
         protected int Litres;
+
         //Ingredients
         protected double Salt, Protein, Fat;
 
@@ -30,11 +32,46 @@ namespace Test
             }
             return Millilitre + litres;
         }
+
+        public virtual void SaltAmount()
+        {
+            double result = Salt - 1;
+            Console.WriteLine(result);
+        }
+
+        public virtual void ProteinAmount()
+        {
+            double result = Protein - 1;
+            Console.WriteLine(result);
+        }
+
+        public virtual void FatAmount()
+        {
+            double result = Fat - 1;
+            Console.WriteLine(result);
+        }
     }
 
     public class DietPepsi : PepsiMax
     {
 
+        public override void SaltAmount()
+        {
+            double result = Salt - 2;
+            Console.WriteLine(result);
+        }
+
+        public override void ProteinAmount()
+        {
+            double result = Protein - 2;
+            Console.WriteLine(result);
+        }
+
+        public override void FatAmount()
+        {
+            double result = Fat - 2;
+            Console.WriteLine(result);
+        }
     }
 
     public class MainClass
@@ -43,6 +80,25 @@ namespace Test
         {
             PepsiMax test = new PepsiMax();
 
+            test.GetIngredients(5,6,7);
+            Console.Write("Salt: ");
+            test.SaltAmount();
+            Console.Write("Protein: ");
+            test.ProteinAmount();
+            Console.Write("Fat: ");
+            test.FatAmount();
+
+            Console.WriteLine("----------");
+
+            DietPepsi test2 = new DietPepsi();
+
+            test2.GetIngredients(10, 9, 8);
+            Console.Write("Salt: ");
+            test2.SaltAmount();
+            Console.Write("Protein: ");
+            test2.ProteinAmount();
+            Console.Write("Fat: ");
+            test2.FatAmount();
         }
     }
 }
